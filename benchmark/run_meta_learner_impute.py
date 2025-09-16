71f1eedff3dd940f2a3a5e1c5e93ac720fbe7a41
Xiaobin Shen <xiaobins@andrew.cmu.edu>
2025-09-16 17:22:37 -0400

    update meta impute run file

---
+++ benchmark/run_meta_learner_impute.py
@@ -0,0 +1,194 @@
+import argparse
+import os
+import sys
+sys.path.insert(1, os.path.dirname(sys.path[0]))
+import pandas as pd
+import numpy as np
+import pickle
+import time
+from tqdm import tqdm
+from models_causal_impute.meta_learners import T_Learner, S_Learner, X_Learner, DR_Learner
+from models_causal_impute.survival_eval_impute import SurvivalEvalImputer
+from data import load_data, prepare_data_split
+
+
+def summarize_experiment_results(results_dict):
+    records = []
+    for config_name, setup_dict in results_dict.items():
+        for scenario_key in setup_dict:
+            row = {
+                ("config_name", ""): config_name,
+                ("scenario_key", ""): scenario_key
+            }
+            for base_model in setup_dict[scenario_key]:
+                avg_result = setup_dict[scenario_key].get(base_model, {}).get("average", {})
+                mean_mse = avg_result.get("mean_cate_mse", np.nan)
+                std_mse = avg_result.get("std_cate_mse", np.nan)
+                mean_ate_pred = avg_result.get("mean_ate_pred", np.nan)
+                std_ate_pred = avg_result.get("std_ate_pred", np.nan)
+                mean_ate_true = avg_result.get("mean_ate_true", np.nan)
+                std_ate_true = avg_result.get("std_ate_true", np.nan)
+                mean_ate_bias = avg_result.get("mean_ate_bias", np.nan)
+                std_ate_bias = avg_result.get("std_ate_bias", np.nan)
+                runtime = avg_result.get("runtime", np.nan)
+
+                row[(base_model, "CATE_MSE")] = f"{mean_mse:.3f} ± {std_mse:.3f}" if not pd.isna(mean_mse) else np.nan
+                row[(base_model, "ATE_pred")] = f"{mean_ate_pred:.3f} ± {std_ate_pred:.3f}" if not pd.isna(mean_ate_pred) else np.nan
+                row[(base_model, "ATE_true")] = f"{mean_ate_true:.3f} ± {std_ate_true:.3f}" if not pd.isna(mean_ate_true) else np.nan
+                row[(base_model, "ATE_bias")] = f"{mean_ate_bias:.3f} ± {std_ate_bias:.3f}" if not pd.isna(mean_ate_bias) else np.nan
+                row[(base_model, "runtime [s]")] = round(runtime) if not pd.isna(runtime) else np.nan
+
+            records.append(row)
+    df = pd.DataFrame.from_records(records)
+    df.columns = pd.MultiIndex.from_tuples(df.columns)
+    return df
+
+def main(args):
+    # TODO: make the following args
+    num_repeats = 10
+    dataset_type = 'actg_syn'
+    cate_true_col = None
+    train_size = 0.5
+    val_size = 0.25
+    test_size = 0.25
+    data_dir = '/heinz-georgenas/users/xiaobins/SurvHTE-Benchmark/data' # TODO: !!!make sure such info is removed for Anonymity!!!
+
+    experiment_setups, experiment_repeat_setups = load_data(dataset_type=dataset_type, data_dir=data_dir)
+
+    output_pickle_path = f"results/synthetic_data/models_causal_impute/meta_learner/{args.meta_learner}/"
+    output_pickle_path += f"{args.meta_learner}_{args.impute_method}_repeats_{args.num_repeats}_train_{args.train_size}.pkl"
+    print("Output results path:", output_pickle_path)
+
+    # base_regressors = ['ridge', 'lasso', 'rf', 'gbr', 'xgb']
+    base_regressors = ['lasso', 'rf', 'xgb']
+    results_dict = {}
+
+    for config_name, setup_dict in tqdm(experiment_setups.items(), desc="Experiment Setups"):
+        results_dict[config_name] = {}
+        for scenario_key in tqdm(setup_dict, desc=f"{config_name} Scenarios"):
+            dataset_df = setup_dict[scenario_key]["dataset"]
+            dataset_summary = setup_dict[scenario_key]["summary"]
+            split_dict = prepare_data_split(
+                dataset_df, experiment_repeat_setups, 
+                num_repeats=num_repeats, 
+                dataset_type=dataset_type,
+                cate_true_col=cate_true_col,
+                train_size=train_size,
+                val_size=val_size,
+                test_size=test_size
+            )
+            results_dict[config_name][scenario_key] = {}
+
+            for base_model in tqdm(base_regressors, desc="Base Models", leave=False):
+                results_dict[config_name][scenario_key][base_model] = {}
+                start_time = time.time()
+
+                for rand_idx in range(num_repeats):
+                    X_train, W_train, Y_train, cate_true_train = split_dict[rand_idx]['train']
+                    X_val, W_val, Y_val, cate_true_val = split_dict[rand_idx]['val']
+                    X_test, W_test, Y_test, cate_true_test = split_dict[rand_idx]['test']
+                    
+                    max_time = Y_train[:, 0].max()
+                    ate_true = dataset_summary['ate']
+                    event_time_25pct = dataset_summary['event_time_25pct']
+                    event_time_50pct = dataset_summary['event_time_median']
+                    event_time_75pct = dataset_summary['event_time_75pct']
+
+                    if args.load_imputed:
+                        with open(args.imputed_path, "rb") as f:
+                            imputed_times = pickle.load(f)
+                        imputed_results = imputed_times.get(args.impute_method, {}).get(config_name, {}).get(scenario_key, {}).get(args.train_size, {}).get(rand_idx, {})
+                        Y_train_imputed = imputed_results.get("Y_train_imputed", None)
+                        Y_test_imputed = imputed_results.get("Y_test_imputed", None)
+                    else:
+                        Y_train_imputed = Y_test_imputed = None
+
+                    if Y_train_imputed is None:
+                        survival_imputer = SurvivalEvalImputer(imputation_method=args.impute_method)
+                        Y_train_imputed, Y_test_imputed = survival_imputer.fit_transform(Y_train, Y_test)
+
+                    if Y_test_imputed is None:
+                        survival_imputer = SurvivalEvalImputer(imputation_method=args.impute_method)
+                        _, Y_test_imputed = survival_imputer.fit_transform(Y_train, Y_test, impute_train=False)
+
+                    if args.meta_learner in ["t_learner", "x_learner"]:
+                        if Y_train[W_train == 1, 1].sum() <= 1:
+                            print(f"[Warning]: For {args.meta_learner}, No event in treatment group. Skipping iteration {rand_idx}.")
+                            continue
+                        if Y_train[W_train == 0, 1].sum() <= 1:
+                            print(f"[Warning]: For {args.meta_learner}, No event in control group. Skipping iteration {rand_idx}.")
+                            continue
+
+                    learner_cls = {"t_learner": T_Learner, "s_learner": S_Learner, "x_learner": X_Learner, "dr_learner": DR_Learner}[args.meta_learner]
+                    learner = learner_cls(base_model_name=base_model)
+
+                    learner.fit(X_train, W_train, Y_train_imputed)
+                    mse_test, cate_test_pred, ate_test_pred = learner.evaluate(X_test, cate_true_test, W_test)
+
+                    # Evaluate base regression models on test data
+                    base_model_eval = learner.evaluate_test(X_test, Y_test_imputed, W_test)
+
+                    results_dict[config_name][scenario_key][base_model][rand_idx] = {
+                        "cate_true": cate_true_test,
+                        "cate_pred": cate_test_pred,
+                        "ate_true": ate_true,
+                        "ate_pred": ate_test_pred.mean_point,
+                        "cate_mse": mse_test,
+                        "ate_bias": ate_test_pred.mean_point - ate_true,
+                        "base_model_eval": base_model_eval, # Store base model evaluation results
+                        "ate_interval": ate_test_pred.conf_int_mean(),
+                        "ate_statistics": ate_test_pred,
+                    }
+
+                end_time = time.time()
+
+                if len(results_dict[config_name][scenario_key][base_model]) == 0:
+                    print(f"[Warning]: No valid results for {config_name}, {scenario_key}, {base_model}. Skipping.")
+                    continue
+
+                # Save results to the setup dictionary
+                avg = results_dict[config_name][scenario_key][base_model]
+                base_model_eval_performance = {
+                                                base_model_k: 
+                                                {
+                                                    f"{stat}_{metric_j}": func([
+                                                        avg[i]['base_model_eval'][base_model_k][metric_j] for i in random_idx_col_list
+                                                        if i in avg
+                                                    ])
+                                                    for metric_j in metric_j_dict
+                                                    for stat, func in zip(['mean', 'std'], [np.nanmean, np.nanstd])
+                                                }
+                                                for base_model_k, metric_j_dict in avg[list(avg.keys())[0]]['base_model_eval'].items()
+                                            }
+                results_dict[config_name][scenario_key][base_model]["average"] = {
+                    "mean_cate_mse": np.mean([avg[i]["cate_mse"] for i in range(num_repeats) if i in avg]),
+                    "std_cate_mse": np.std([avg[i]["cate_mse"] for i in range(num_repeats) if i in avg]),
+                    "mean_ate_pred": np.mean([avg[i]["ate_pred"] for i in range(num_repeats) if i in avg]),
+                    "std_ate_pred": np.std([avg[i]["ate_pred"] for i in range(num_repeats) if i in avg]),
+                    "mean_ate_true": np.mean([avg[i]["ate_true"] for i in range(num_repeats) if i in avg]),
+                    "std_ate_true": np.std([avg[i]["ate_true"] for i in range(num_repeats) if i in avg]),
+                    "mean_ate_bias": np.mean([avg[i]["ate_bias"] for i in range(num_repeats) if i in avg]),
+                    "std_ate_bias": np.std([avg[i]["ate_bias"] for i in range(num_repeats) if i in avg]),
+                    "runtime": (end_time - start_time) / len(avg) if len(avg) > 0 else 0,
+                    "base_model_eval": base_model_eval_performance
+                }
+
+            with open(output_pickle_path, "wb") as f:
+                pickle.dump(results_dict, f)
+            # break
+        # break
+
+    # df = summarize_experiment_results(results_dict)
+    # print(df)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_repeats", type=int, default=10)
+    parser.add_argument("--train_size", type=int, default=5000)
+    parser.add_argument("--test_size", type=int, default=5000)
+    parser.add_argument("--impute_method", type=str, default="Pseudo_obs", choices=["Pseudo_obs", "Margin", "IPCW-T"])
+    parser.add_argument("--meta_learner", type=str, default="t_learner", choices=["t_learner", "s_learner", "x_learner", "dr_learner"])
+    parser.add_argument("--load_imputed", action="store_true")
+    parser.add_argument("--imputed_path", type=str, default="synthetic_data/imputed_times_lookup.pkl")
+    args = parser.parse_args()
+    main(args)
